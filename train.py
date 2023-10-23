aac5fbbdd7c5fc4ca84b7e08f0a858c561e32465
estelle houndjo <lawaniaduni75@gmail.com>
2023-10-23 17:45:26 +0200

    add files and first notes

---
+++ train.py
@@ -0,0 +1,14 @@
+# https://www.malekal.com/les-commandes-de-base-invite-de-commande-cmd-windows/#Comment_creer_un_nouveau_repertoire_avec_une_invite_de_commande_CMD
+# Invite de commande windows
+print("Hello")
+
+# Création du stage area (studio de photos) : git init > cela créé un dossier caché .git
+
+# git status : voir l'état des choses
+
+# Ajouter un fichier au studio de photos : git add <fichier>
+
+# Il peut y avoir des fichiers ou dossiers que je ne veux pas suivre (avec Git) : 
+# Je créer alors le fichier .gitignore et je les y insère
+
+# git rm -rf --cached : Enlevez l'ensemble des fichiers qui se trouvent dans le studio
